c7a1ecdbd061e1a8b6dfcbf741e4fb7ea3a59944
Will Fitzgerald <37049+willf@users.noreply.github.com>
2022-12-04 15:42:57 -0500

    use begin

--- 04/allen_algebra.rb
+++ 04/allen_algebra.rb
@@ -1,58 +1,58 @@
 module AllenAlgebra
   def precedes?(other)
-    self.end < other.first
+    self.end < other.begin
   end
 
   def meets?(other)
-    self.end == other.first
+    self.end == other.begin
   end
 
   def overlaps?(other)
-    self.end > other.first && self.end < other.end
+    self.end > other.begin && self.end < other.end
   end
 
   def finished_by?(other)
-    self.end == other.end && self.first < other.first
+    self.end == other.end && self.begin < other.begin
   end
 
   def contains?(other)
-    self.first < other.first && self.end > other.end
+    self.begin < other.begin && self.end > other.end
   end
 
-  def firsts?(other)
-    self.first == other.first && self.end < other.end
+  def begins?(other)
+    self.begin == other.begin && self.end < other.end
   end
 
   def equals?(other)
-    self.first == other.first && self.end == other.end
+    self.begin == other.begin && self.end == other.end
   end
 
-  def firsted_by?(other)
-    self.first == other.first && self.end > other.end
+  def begined_by?(other)
+    self.begin == other.begin && self.end > other.end
   end
 
   def during?(other)
-    self.first > other.first && self.end < other.end
+    self.begin > other.begin && self.end < other.end
   end
 
   def finishes?(other)
-    self.first > other.first && self.end == other.end
+    self.begin > other.begin && self.end == other.end
   end
 
   def overlapped_by?(other)
-    self.first < other.first && self.end > other.first && self.end < other.end
+    self.begin < other.begin && self.end > other.begin && self.end < other.end
   end
 
   def met_by?(other)
-    self.first == other.end
+    self.begin == other.end
   end
 
   def preceded_by?(other)
-    self.first > other.end
+    self.begin > other.end
   end
 
   def commpletely_includes?(other)
-    self.first <= other.first && self.end >= other.end
+    self.begin <= other.begin && self.end >= other.end
   end
 
   def overlaps_at_all?(other)
